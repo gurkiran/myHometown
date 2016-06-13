26317dee13dfdd22747cd693dc0bfad8fd816265
gurkiran <gurkiran4you@gmail.com>
2016-06-12 20:44:30 -0400

    cleaning up

--- public/controllers/gallery.js
+++ public/controllers/gallery.js
@@ -186,41 +186,6 @@
 
         displayTime();
 
-
-
-   /* $("#btn").click(function(){
-            var city = $("#cityName").val();
-            var geocoder =  new google.maps.Geocoder();
-            geocoder.geocode( { 'address': city}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-
-            var latitude = results[0].geometry.location.lat();
-            var longitude = results[0].geometry.location.lng()
-
-            document.getElementById("show1").innerHTML=latitude.toFixed(2);
-            document.getElementById("show2").innerHTML=longitude.toFixed(2);
-
-            $("#cordinates").attr('title','Significance').dialog({
-            modal:true,
-            width:500,
-            height:500,
-            buttons: {
-                "OK": function() {
-                    $(this).dialog('close');
-            }
-        },
-            draggable:true,
-            resizable:true
-
-        });
-
-          } else {
-            alert("Something got wrong " + status);
-          }
-        });
-
-});    */
-
   $('#ehvi').accordion({
     collapsible : true,
     heightStyle : 'content'
@@ -265,46 +230,6 @@
         xmlhttp.open("GET", url, true);
         xmlhttp.send();
     }
-    /*
-    function degreesToDirection(degrees){
-        if(degrees >= 348.75 && degrees < 11.25)
-        return "N";
-        if(degrees >= 11.25 && degrees < 33.75)
-        return "NNE";
-        if(degrees >= 33.75  && degrees < 56.25)
-        return "NE";
-        if(degrees >= 56.25  && degrees < 78.75)
-        return "ENE";
-        if(degrees >= 78.75  && degrees < 101.25)
-        return "E";
-        if(degrees >= 101.25  && degrees < 123.75)
-        return "SE";
-        if(degrees >= 123.75  && degrees < 146.25)
-        return "SSE";
-        if(degrees >= 146.25  && degrees < 168.75)
-        return "S";
-        if(degrees >= 191.25  && degrees < 213.75)
-        return "SSW";
-        if(degrees >= 213.75 && degrees < 236.25)
-        return "SW";
-        if(degrees >= 236.25  && degrees < 258.75)
-        return "WSW";
-        if(degrees >= 258.75  && degrees < 281.25)
-        return "W";
-        if(degrees >= 281.25  && degrees < 303.75)
-        return "WNW";
-        if(degrees >= 303.75  && degrees < 326.25)
-        return "NW";
-        if(degrees >= 326.25  && degrees < 348.75)
-        return "NNW";
-
-    }
-
-
-    function kelvinToFahrenheit(k){
-        return Math.round(k*(9/5)-459.67);
-    }
-    */
 
     function kelvinToCelsius(k){
         return Math.round(k - 273.15);
@@ -312,32 +237,15 @@
 
 
     function update(weather) {
-       /* icon.src = "codes/" + weather.weather[0].id + ".png"
-        humidity.innerHTML = weather.main.humidity;
-        wind.innerHTML =weather.wind.speed;
-        direction.innerHTML = degreesToDirection(weather.wind.deg);
-        loc.innerHTML = weather.name;*/
         description.innerHTML=weather.weather[0].description;
         temp.innerHTML =kelvinToCelsius(weather.main.temp);
-     /*   temp.innerHTML =kelvinToFahrenheit(weather.main.temp);   */
-
     }
 
 
     window.onload = function () {
         temp = document.getElementById("temperature");
-      /*  loc = document.getElementById("location");
-        icon = document.getElementById("icon");
-        humidity = document.getElementById("humidity");
-        wind = document.getElementById("wind");
-        direction = document.getElementById("direction"); */
         description= document.getElementById("description");
 
-     /*   icon.src = "codes/300.png";
-        humidity.innerHTML = "60";
-        wind.innerHTML= "Hello";
-        direction.innerHTML = "SE";
-        loc.innerHTML = "Batala";*/
         temp.innerHTML = "25";
         description.innerHTML= "";
 
